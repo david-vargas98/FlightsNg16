bfd93abc5165172c0aa4929c4ed3325518078914
david-vargas98 <edgar981125@live.com.mx>
2025-08-20 17:41:12 -0600

    fix: from input corrections.
    The 'formControlName' source was corrected and replaced for 'from' as well as it was made to the component.
    
    The search() method now gets executed when the hook lifecycle 'ngOnInit' is executed.

--- flights.client/src/app/search-flights/search-flights.component.ts
+++ flights.client/src/app/search-flights/search-flights.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FlightService } from '../api/services/flight.service';
 import { FlightRm } from '../api/models';
 import { FormBuilder } from '@angular/forms';
@@ -9,19 +9,23 @@ import { SearchFlight$Params } from '../api/fn/flight/search-flight';
   templateUrl: './search-flights.component.html',
   styleUrls: ['./search-flights.component.css']
 })
-export class SearchFlightsComponent {
+export class SearchFlightsComponent implements OnInit {
 
   searchResults: FlightRm[] = [];
 
   constructor(private flightService: FlightService, private formBuilder: FormBuilder) { }
 
   searchForm = this.formBuilder.group({
-    source: [''],
+    from: [''],
     destination: [''],
     fromDate: [''],
     toDate: [''],
     numberOfPassengers: [1]
   });
+
+  ngOnInit() {
+    this.search();
+  }
 
   search() {
     this.flightService.searchFlight(this.searchForm.value as SearchFlight$Params)
