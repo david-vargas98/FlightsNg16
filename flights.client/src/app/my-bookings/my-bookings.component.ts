ee992b8c6ee0d4e335e64f3abc1377647c7148dd
david-vargas98 <edgar981125@live.com.mx>
2025-08-20 00:54:32 -0600

    feat: Implementing redirect if a user is not log in.

--- flights.client/src/app/my-bookings/my-bookings.component.ts
+++ flights.client/src/app/my-bookings/my-bookings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BookingRm } from '../api/models';
 import { BookingService } from './../api/services/booking.service';
 import { AuthService } from './../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-bookings',
@@ -12,11 +13,16 @@ export class MyBookingsComponent implements OnInit {
   
   bookings!: BookingRm[]; // The "!" tells the compiler that cannot be null or undefined
 
-  constructor(private bookingService: BookingService, private authService: AuthService) { }
+  constructor(private bookingService: BookingService, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+
+    if (!this.authService.currentUser?.email)
+      this.router.navigate(['/register-passenger']);
+
     this.bookingService.listBooking({ email: this.authService.currentUser?.email ?? '' })
       .subscribe(result => this.bookings = result, this.handleError);
+
   }
 
   private handleError(err: any) {
