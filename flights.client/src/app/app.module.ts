591a65b5a233c6393f2aa3acc42370693fbe0b20
david-vargas98 <edgar981125@live.com.mx>
2025-08-20 10:14:26 -0600

    feat: Auth guard.
    Creation of auth.guard and addition to app.module.

--- flights.client/src/app/app.module.ts
+++ flights.client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { BookFlightComponent } from './book-flight/book-flight.component';
 import { RegisterPassengerComponent } from './register-passenger/register-passenger.component';
 import { MyBookingsComponent } from './my-bookings/my-bookings.component';
+import { authGuard } from './auth/auth.guard';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,7 @@ import { MyBookingsComponent } from './my-bookings/my-bookings.component';
     RouterModule.forRoot([
       { path: '', component: SearchFlightsComponent, pathMatch: 'full' },
       { path: 'search-flights', component: SearchFlightsComponent },
-      { path: 'book-flight/:flightId', component: BookFlightComponent },
+      { path: 'book-flight/:flightId', component: BookFlightComponent, canActivate: [authGuard] },
       { path: 'register-passenger', component: RegisterPassengerComponent },
       { path: 'my-booking', component: MyBookingsComponent }
     ])
