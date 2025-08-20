aaaac5c5a640a0bf5e4dd7429b6aa7abe58ea142
david-vargas98 <edgar981125@live.com.mx>
2025-08-20 11:51:38 -0600

    feat/refactor: auth.guard for authorization.
    Creating an auth.guard to move the authorization logic from book-flight to it and avoid repeting myself.
    
    Also, the const authGuard is included in 'canActivate' to use the guard.
    
    This centralize the logic into the guard instead of repeating the same thing into every component.

--- flights.client/src/app/auth/auth.guard.ts
+++ flights.client/src/app/auth/auth.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.currentUser) {
+    return router.parseUrl('/register-passenger'); // better than navigate, since it doesn't attempt double navigations
+  }
+
   return true;
 };
